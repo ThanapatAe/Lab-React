ff946ceaec0d04e897bc8688b6027a7ebfa0c3d6
ThanapatAe <47793299+ThanapatAe@users.noreply.github.com>
2019-09-27 17:00:11 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+
+
+class App extends Component {
+    render() {
+        return ( <div>
+            
+                <WordCard value="hello"/>
+   
+            </div>
+        );
+    }
+}
+
+
+export default App;
